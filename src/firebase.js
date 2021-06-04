ac4823deed1f56359643c623385c3fe4dfb5206a
jinyanomura <jinjinjinjinjin.n@gmail.com>
2021-06-04 21:43:52 +0900

    done firebase deploying

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDi4SDXgZ0kwdtsT0LHrP8f2ZIcE9oK2ag",
-  authDomain: "monster-slayer-5d861.firebaseapp.com",
-  projectId: "monster-slayer-5d861",
-  storageBucket: "monster-slayer-5d861.appspot.com",
-  messagingSenderId: "532855348128",
-  appId: "1:532855348128:web:d3dcf8d4e580b7680f62c5",
-  measurementId: "G-JX9PTY7C94",
+  apiKey: "AIzaSyBst22DQe_OzeF9ahd0hbnlvM7u0yUidG0",
+  authDomain: "monster-slayer-8f17f.firebaseapp.com",
+  projectId: "monster-slayer-8f17f",
+  storageBucket: "monster-slayer-8f17f.appspot.com",
+  messagingSenderId: "1020366263689",
+  appId: "1:1020366263689:web:c5c5bf7c20183c0d467d58",
+  measurementId: "G-2M96P9T1FD",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
